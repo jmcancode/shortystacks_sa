152568ef8ef5e572248af1387daec40bc66dc5d5
Jon-Michael <narvaez.jonmichael@gmail.com>
2021-11-11 18:49:20 -0600

    images

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,23 +1,36 @@
-
 import React, {Fragment} from "react";
 import menu from "../assets/Unknown.png"
+import CarouselImage from "../components/carousel/CarouselImage";
 
 export default function Menu() {
     return (
-        <>
-            <div
-                style={{
-                display: 'flex',
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: 'center',
-                height: "auto"
-                
-            }}>
-
-                <img src={menu} alt="menu" width={100} height={100} style={{width: "90%", height: "100%"}}/>
-
+        <Fragment>
+            <div className="row">
+                <div
+                    className="col"
+                    style={{
+                    display: 'flex',
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: 'center',
+                    height: "auto"
+                }}>
+                    <img
+                        src={menu}
+                        alt="menu"
+                        width={100}
+                        height={100}
+                        style={{
+                        width: "90%",
+                        height: "100%"
+                    }}/>
+                </div>
             </div>
-        </>
+            <div className="row mb-5">
+                <div className="col">
+                    <CarouselImage/>
+                </div>
+            </div>
+        </Fragment>
     );
 }
